784a7ab959449af68defa47cd96fb27676476cbb
shehab/1999 <alaamerishehab@gmail.com>
2025-05-02 15:58:17 +0300

    3

--- next.config.ts
+++ next.config.ts
@@ -4,15 +4,7 @@ const nextConfig: NextConfig = {
     async redirects() {
         return [
            
-            {
-                source: '/Main', // أي مسار غير موجود
-                destination: '/main2', // الصفحة المخصصة
-                permanent: false,
-              },{
-                source: '/', // أي مسار غير موجود
-                destination: '/main1', // الصفحة المخصصة
-                permanent: false,
-              },
+           
          
         ];
       },
